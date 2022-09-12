be90b994576feb457f1f7ae1c987dadbdd79c828
Mohammad-Aghaei <111427824+Mohammad-Aghai@users.noreply.github.com>
2022-09-12 14:56:02 +0430

    Add files via upload

---
+++ scroll.js
@@ -0,0 +1,148 @@
+window.sr = ScrollReveal();
+sr.reveal('nav', {
+                        duration: 1000,
+                        origin: 'bottom',
+                        distance: '40px'
+});
+sr.reveal('.navleft', {
+                        duration: 1500,
+                        origin: 'left',
+                        distance: '40px'
+});
+sr.reveal('.navright', {
+                        duration: 1500,
+                        origin: 'right',
+                        distance: '40px'
+});
+sr.reveal('#bgparent', {
+                        duration: 1500,
+                        origin: 'top',
+                        distance: '40px'
+});
+sr.reveal('.txtparent', {
+                        duration: 3000,
+                        origin: 'bottom',
+
+});
+sr.reveal('.body1', {
+                        duration: 1500,
+                        origin: 'left',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.body2', {
+                        duration: 1500,
+                        origin: 'right',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.body3', {
+                        duration: 1500,
+                        origin: 'right',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.footer1', {
+                        duration: 1500,
+                        origin: 'right',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.footer2', {
+                        duration: 1500,
+                        origin: 'left',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.footer3', {
+                        duration: 1500,
+                        origin: 'bottom',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('#foote', {
+                        duration: 1500,
+                        origin: 'top',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+})
+sr.reveal('#hedparent', {
+                        duration: 1000,
+                        origin: 'top',
+
+                        distance: '200px'
+
+})
+sr.reveal('#hedchild', {
+                        duration: 1500,
+                        distance: '60px'
+
+});
+sr.reveal('.anima', {
+                        duration: 1000,
+                        viewfactor: 0.2,
+                        distance: '100px'
+
+});
+sr.reveal('#leftabout', {
+                        duration: 1500,
+                        origin: 'left',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('#centerabout', {
+                        duration: 1500,
+                        origin: 'top',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('#rightabout', {
+                        duration: 1500,
+                        origin: 'right',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.grid-right', {
+                        duration: 1500,
+                        origin: 'right',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.flex-left', {
+                        duration: 1500,
+                        origin: 'left',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.sass-center', {
+                        duration: 1500,
+                        origin: 'bottom',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+sr.reveal('.botstrap-center', {
+                        duration: 1500,
+                        origin: 'bottom',
+                        viewfactor: 0.2,
+                        distance: '40px'
+
+});
+
+
+  
+
+
+
